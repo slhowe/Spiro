d9771914f3bd3a3eb3cb414f5097e07dea64d85d
= <sarah.howe@pg.canterbury.ac.nz>
2016-04-20 13:26:47 +1200

    "Now fitting LSQ to a %age of flow drop"

--- Spirometry.m
+++ Spirometry.m
@@ -20,20 +20,8 @@ band = data.Banding;
 normal = data.Normal;
 inflated = data.Inflated;
 
-% start loops
-flow = loops.Flow;
-pressure = loops.Pressure;
-
 % sampling frequency 125 Hz
 Hz = 125;
-
-% time for plotting
-time = (1:size(flow))*(1/Hz);
-
-% Calc volume.
-% This drifts over many breaths
-% best to go breath to breath
-vol = cumtrapz(flow*(1/Hz));
 
 % Atmospheric pressure in kpa
 p_atm = 101.35; %kPa
@@ -46,6 +34,12 @@ p_atm = 101.35; %kPa
 % Resistance is resistance
 % Compliance is capacitance
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% start loops
+flow = loops.Flow;
+pressure = loops.Pressure;
+
+% time for plotting
+time = (1:size(flow))*(1/Hz);
 
 % Looks like an RC curve at end of forced exp?
 % Cause? Lung relaxing after muscles release???
@@ -66,8 +60,8 @@ hold off
 curveStart = start;
 curveDataEnd = stop;
 
-% set stopping point of fit at 90% flow drop
-drop = 0.9*flow(curveStart)-flow(curveDataEnd);
+% set stopping point of fit at certain %age flow drop
+drop = 0.1*(flow(curveStart)-flow(curveDataEnd));
 
 % find the index of the stopping point
 index = 0;
@@ -80,7 +74,10 @@ for i = curveStart:curveDataEnd
         end
     end
 end
-curveStop = CurveStart+index;
+if index == 0
+    error("Percentage flow drop specified not found in range")
+end
+curveStop = index;
 
 % set up matrices
 measurements = log(-flow(curveStart:curveStop));
@@ -103,55 +100,3 @@ hold on
 plot(-newValues, 'm')
 legend("original", "LSQ fit");
 hold off
-
-
-%E = zeros(1,1200);
-%R = zeros(1,1200);
-%pi = p_atm;
-%for i = 4:1200
-%    vi = vol(i) - offset;
-%    if vi == 0
-%        E(i) = 0;
-%    else
-%        E(i) = pi/vi;
-%    endif
-%    params = [vol(i-3:i)-offset, flow(i-3:i)]\(pi*ones(4, 1));
-%    E(i) = params(1);
-%    R(i) = params(2);
-%endfor
-
-%figure
-%hold on
-%subplot(4, 1, 1)
-%hold on
-%plot(time(1:1200), flow(1:1200))
-%plot(time(740), flow(740), 'rx')
-%hold off
-%grid minor
-%legend("flow")
-%ylabel("l/s")
-%xlabel("s")
-%subplot(4, 1, 2)
-%hold on
-%plot(time(1:1200), vol(1:1200)-offset+3)
-%plot(time(740), vol(740)-offset, 'rx')
-%hold off
-%legend("volume")
-%grid minor
-%ylabel("L")
-%xlabel("s")
-%subplot(4, 1, 3)
-%hold on
-%plot(time(1:1200), E)
-%plot(time(740), E(740), 'rx')
-%hold off
-%grid minor
-%ylabel("E")
-%subplot(4, 1, 4)
-%hold on
-%plot(time(1:1200), R)
-%plot(time(740), R(740), 'rx')
-%hold off
-%grid minor
-%ylabel("R")
-
