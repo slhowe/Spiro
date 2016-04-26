e6a3981632e583d6a2dff5940d02ce4cac343193
= <sarah.howe@pg.canterbury.ac.nz>
2016-04-27 11:24:04 +1200

    Oops, had the greater and less than signs the wrong way around XD

--- checkIndicesAreForSameBreath.m
+++ checkIndicesAreForSameBreath.m
@@ -13,13 +13,11 @@ function [pressure_pair, flow_pair] = checkIndicesAreForSameBreath(pressure_pair
         pressure_start = pressure_splits(1, pressure_pair);
         flow_start = flow_splits(1, flow_pair);
         % Check if they are close enough
-        % They will be within a half breath from
-        % each other with pressure leading
-        if((abs(pressure_start - flow_start) < breath_length/2) && (pressure_start < flow_start))
+        if(abs(pressure_start - flow_start) < breath_length/2)
             noMatch = 0;
         else
             % Go to next pressure index if flow leads
-            if(pressure_start > flow_start)
+            if(pressure_start < flow_start)
                 pressure_pair = pressure_pair + 1;
                 if(pressure_pair > length(pressure_splits))
                     noMatch = 0;
