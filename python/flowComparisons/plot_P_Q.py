355559d0532b12839d3949e23c77d49ca0072190
= <sarah.howe@pg.canterbury.ac.nz>
2016-09-22 14:12:26 +1200

    Add example code

---
+++ python/flowComparisons/plot_P_Q.py
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Import my extensions
+import sys
+sys.path.insert(0, '/home/sarah/Documents/Spirometry/python/extensions')
+
+import csv
+import matplotlib.pyplot as plt
+from filters import semi_gauss_lp_filter
+from data_struct import DataStore as Data
+from numpy import array, sign
+from numpy.linalg import lstsq
+
+path = '/home/sarah/Documents/Spirometry/python/flowComparisons/'
+files = ['Calibration_set']
+#path = '/home/sarah/Documents/Spirometry/data/'
+#files = ['Loops_1.csv']
+
+# Create data classes
+reset = Data('reset')
+reset.clear_dataset_registry()
+dataset_1 = Data('dataset_1')
+dataset_2 = Data('dataset_2')
+
+# Points I prepared earlier =D
+dataset_2_start_indices = [2465, 5737, 8939]
+dataset_2_end_indices = [2865, 6150, 9340]
+
+# Store pressure and flow data
+file_index = 0
+for dataset in Data:
+    filename = path + files[file_index]
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+
+        # skip header line
+        header = reader.next()
+
+        for row in reader:
+            pressure = float(row[0])
+            flow = float(row[1])
+
+            dataset.pressure.append(pressure)
+            dataset.flow.append(flow)
+    file_index += 1
+
+    offset = 26
+
+    # measured
+    pressure = dataset.pressure[0:-offset]
+    flow = dataset.flow[offset:]
+    fpressure = semi_gauss_lp_filter(pressure, 125, 1, plot=True)
+
+    plt.plot(pressure, 'b',
+             flow, 'g',
+             fpressure, 'r',
+             )
+
+    plt.grid()
+    plt.legend(["Pressure",
+                "Flow"
+                ])
+    plt.show()
