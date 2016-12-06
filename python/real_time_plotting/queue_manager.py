7ba81e88764582e713a3539cac2ba58f93e76804
= <sarah.howe@pg.canterbury.ac.nz>
2016-12-07 12:35:23 +1300

    Added a queue clearing function

--- python/real_time_plotting/queue_manager.py
+++ python/real_time_plotting/queue_manager.py
@@ -12,10 +12,18 @@ queue
 
 def get_queue_item(queue_object):
     try:
-        item = queue_object.get(False)
+        item = queue_object.get(True, 0.05)
     except Queue.Empty:
+        print('Delay in reading queue')
         return None
     return item
+
+def clear_queue(queue_object):
+    while True:
+        try:
+            yield q.get_nowait()
+        except Queue.Empty:
+            break
 
 def get_all_queue_items(queue_object):
     pass
