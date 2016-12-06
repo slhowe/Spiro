1752126352f8344c570d852c929b6a7e0db9c41b
= <sarah.howe@pg.canterbury.ac.nz>
2016-12-07 12:39:04 +1300

    New threading class to analyse data

---
+++ python/real_time_plotting/analyser.py
@@ -0,0 +1,35 @@
+import Queue
+import threading
+
+class ExprDecayAnalyser(threading.Thread):
+    """
+    A thread for analysing expiration. Looks for
+    exponential decays in expiration data.
+    """
+    def __init__(self,
+                 data_q
+                 ):
+        threading.Thread.__init__(self)
+
+        self.data_q = data_q
+
+        # Internal flag for inter-thread communication
+        self.alive = threading.Event()
+        self.alive.set()
+
+        print('Analyser setup')
+
+    def run(self):
+        # Any setup required at runtime goes here
+
+        while self.alive.isSet():
+            # Read queue to list
+
+            # Find peak
+
+            # Find low point
+
+            # Fit exp decay
+
+            # Results
+            pass
