a80443453fc1c4fd7cef47950cee299c0c9e1132
= <sarah.howe@pg.canterbury.ac.nz>
2018-06-26 10:15:19 +1200

    analysis plots data

--- python/mask_LPF/find_mask_spir_relationship.py
+++ python/mask_LPF/find_mask_spir_relationship.py
@@ -15,47 +15,50 @@ SENSOR_CUTOFF = 0.29
 
 #filename = './data_recording.csv'
 #filename = './breath_example_without_Rx2.csv'
-filenames = ['../Ps_Pm_calibration/mask_RX1.csv',
-        '../Ps_Pm_calibration/mask_RX2.csv',
-        '../Ps_Pm_calibration/mask_woRX1.csv',
-        '../Ps_Pm_calibration/mask_woRX2.csv',
+filenames = [
         '../Ps_Pm_calibration/mp_RX1.csv',
         '../Ps_Pm_calibration/mp_RX2.csv',
         '../Ps_Pm_calibration/mp_woRX1.csv',
-        '../Ps_Pm_calibration/mp_wpRX2.csv',
-]
+        '../Ps_Pm_calibration/mp_woRX2.csv',
+        '../Ps_Pm_calibration/mask_RX1.csv',
+        '../Ps_Pm_calibration/mask_RX2.csv',
+        '../Ps_Pm_calibration/mask_woRX1.csv',
+        '../Ps_Pm_calibration/mask_woRX2.csv',
+        ]
 
 pressure = []
 
 for filename in filenames:
     with open(filename, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',')
-
         # skip header line
         header = reader.next()
-
         for row in reader:
             # Values read as string. Need converting
-            spir = -float(row[1])
-            mask = -float(row[0])
-
+            spir = float(row[1])
+            mask = float(row[0])
             # Pressures linked in tuples for later sorting
             pressure.append((spir, mask))
 
     # Sorted in ascending order by mask pressure
     # Because mask pressure will hit pressure cutoff first
     pressure.sort(key=lambda tup: tup[1])
-
     spir_pressure = []
     mask_pressure = []
-
     # Sensor cuts off
     for i in range(len(pressure)):
         spir_pressure.append(pressure[i][0])
         mask_pressure.append(pressure[i][1])
 
+
     # relationship in expiration
 
+    # Data points to generate lines for plotting
+    curve_l = range(-0, -150, -1)
+    curve_l = [c/1000.0 for c in curve_l]
+
+    # Quadratic relationship?
+    # FIT TO ALL OF EXPIRATION RANGE
     # FINDING EXPIRATION RANGE
     # Find first index less than 0
     # find first index less than -0.29
@@ -74,9 +77,6 @@ for filename in filenames:
                 i = num_datapoints
         i+=1
 
-
-    # Quadratic relationship?
-    # FIT TO ALL OF EXPIRATION RANGE
     one_array = [1]*(end-start)
     spir_array = spir_pressure[start:end]
     mask_array = mask_pressure[start:end]
@@ -84,18 +84,18 @@ for filename in filenames:
 
     independents = array([spir_array_sqd, spir_array, one_array])
     dependents = array(mask_array)
-
     result = lstsq(independents.T, dependents.T)
+    print("Quadratic all: ")
     print(result)
+    print(" ")
 
-    curve = range(0, -150, -1)
-    curve = [c/1000.0 for c in curve]
-    data = [result[0][0]*(c**2) + result[0][1]*c + result[0][2] for c in curve]
-    data2 = [-6.4*(c**2) + c for c in curve]
+    data = [result[0][0]*(c**2) + result[0][1]*c + result[0][2] for c in curve_l]
+    data2 = [-6.4*(c**2) + c for c in curve_l]
 
 
     #linear relationship
 
+    # Fit to high pressure range
     start = -1
     end = -1
     i = 0
@@ -116,26 +116,57 @@ for filename in filenames:
 
     independents = array([spir_array_l, one_array_l])
     dependents = array(mask_array_l)
-
     result = lstsq(independents.T, dependents.T)
+    print("High linear: ")
     print(result)
+    print(" ")
 
-    curve_l = range(-20, -150, -1)
-    curve_l = [c/1000.0 for c in curve_l]
-    data_l = [result[0][0]*c + result[0][1] for c in curve_l]
-    data_l2 = [1.8*c + 0.03 for c in curve_l]
+    data_high = [result[0][0]*c + result[0][1] for c in curve_l]
 
-    # 1-1 line
-    curve_1t1 = range(-150, 260, 10)
-    curve_1t1 = [c/1000.0 for c in curve_1t1]
+    # Fit to low pressure range
+    start = -1
+    end = -1
+    i = 0
+    num_datapoints = len(mask_pressure)
+    while i < num_datapoints:
+        if(start < 0):
+            if(mask_pressure[i] > -0.02):
+                start = i
+        else:
+            if(mask_pressure[i] >= -0.0):
+                end = i - 1
+                i = num_datapoints
+        i+=1
+
+    one_array_l = [1]*(end-start)
+    spir_array_l = spir_pressure[start:end]
+    mask_array_l = mask_pressure[start:end]
+
+    independents = array([spir_array_l, one_array_l])
+    dependents = array(mask_array_l)
+    result = lstsq(independents.T, dependents.T)
+    print("Low linear: ")
+    print(result)
+    print(" ")
+
+    data_low = [result[0][0]*c + result[0][1] for c in curve_l]
+
+   # 1-1 line
+    curve_1t1 = [c/1000.0 for c in curve_l]
+
+    # low line mp
+    curve_RX = [2.95*c + 0.05 for c in curve_l]
+    curve_woRX= [2.5*c + 0.03 for c in curve_l]
 
     plt.plot(mask_pressure, spir_pressure, 'x')
     plt.plot(mask_array, spir_array, 'mx')
-    plt.plot(data, curve, 'r')
-    plt.plot(data2, curve, 'g')
-    plt.plot(data_l, curve_l, 'k', linewidth=3)
-    plt.plot(data_l2, curve_l, ':k', linewidth=3)
-    plt.plot(curve_1t1, curve_1t1, 'k', linewidth=3)
+    plt.plot(data, curve_l, 'r')
+    plt.plot(data2, curve_l, 'g')
+    plt.plot(data_high, curve_l, 'k', linewidth=3)
+    plt.plot(data_low, curve_l, 'k', linewidth=3)
+    plt.plot(curve_1t1, curve_1t1, 'k:', linewidth=3)
+    plt.plot(curve_RX, curve_l, 'g', linewidth=3)
+    plt.plot(curve_woRX, curve_l, 'c', linewidth=3)
     plt.xlabel('mask')
     plt.ylabel('spir')
     plt.show()
--- python/office_trial/data_analysis.py
+++ python/office_trial/data_analysis.py
@@ -9,24 +9,30 @@ import matplotlib.pyplot as plt
 from data_struct import DataStore as Data
 from calculus import integral
 
-path = '/home/sarah/Documents/Spirometry/python/'
+path = './trial_data/'
+subject = [
+          'F01',
+          ]
 files = [
-        'data_recording.csv'
+        '_mp_noRx.csv',
+        '_mp_Rx.csv',
          ]
 
-pressure_lowRes = []
-pressure_highRes = []
-time = []
+# Coefficients for Ps -> Pm
+# Pm = Ps*c[0] + c[1]
+# [mp_RX, mp_woRX]
+conversion = [
+        [2.9, 0.05],
+        [2.5, 0.03],
+        ]
+
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Extract pressure two data arrays (assumed pressure and flow,
 # but could be pressure & pressure etc) and time stamp from csv
+# NOTE This function appends. Make sure empty arrays are passed in
 def extract_data_arrays_from_file(filename, pressure, flow, time):
-    pressure = []
-    flow = []
-    time = []
-
     # Store pressure and flow data
     with open(filename, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',')
@@ -58,12 +64,36 @@ def convert_spirometer_pressure_to_flow(pressure):
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Calibration for this conversion is in
-# ~/Documents/Spirometry/python/maskLPF
-def convert_spirometer_pressure_to_mouth_pressure(spir_pressure):
-    pass
+# ~/Documents/Spirometry/python/maskLPF/Ps_Pm_calibration
+def convert_spirometer_pressure_to_mouth_pressure(spir_pressure, coeffs):
+    mouth_pressure = [coeffs[0]*p + coeffs[1] for p in spir_pressure]
+    return mouth_pressure
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-for i in range(len(files)):
-    filename = path + files[i]
-    extract_data_arrays_from_file(filename, pressure_lowRes, pressure_highRes, time)
+for j in range(len(subject)):
+    for i in range(len(files)):
+        pressure_lowRes = []
+        pressure_highRes = []
+        time = []
+
+        filename = path + subject[j] + '/' + subject[j] + files[i]
+        extract_data_arrays_from_file(filename, pressure_lowRes, pressure_highRes, time)
+
+        flow = convert_spirometer_pressure_to_flow(pressure_highRes)
+        mouth_pressure = convert_spirometer_pressure_to_mouth_pressure(pressure_highRes, conversion[i])
+
+        plt.plot(time, pressure_lowRes, 'b', linewidth=2)
+        plt.plot(time, pressure_highRes, 'k', linewidth=2)
+        plt.xlabel('Time(s)')
+        plt.ylabel('Pressure(Pa)')
+        plt.legend(['low_res', 'high_res'])
+        plt.show()
+
+        plt.plot(time, flow, 'b', linewidth=2)
+        plt.plot(time, pressure_highRes, 'm', linewidth=2)
+        plt.plot(time, mouth_pressure, 'k', linewidth=2)
+        plt.xlabel('Time(s)')
+        plt.ylabel('Pressure(Pa)')
+        plt.legend(['flow', 'high_res', 'mouth_pressure'])
+        plt.show()
