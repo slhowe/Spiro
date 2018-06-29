0437630c2bfa8a231014e5c9b33af0ff94f2582c
= <sarah.howe@pg.canterbury.ac.nz>
2018-06-29 14:38:11 +1200

    starting on correct Ps to Pm conversion

--- python/office_trial/data_analysis.py
+++ python/office_trial/data_analysis.py
@@ -8,22 +8,34 @@ import csv
 import matplotlib.pyplot as plt
 from data_struct import DataStore as Data
 from calculus import integral
+import numpy as np
 
 path = './trial_data/'
 subject = [
           'F01',
           ]
-files = [
+test_type = [
+        [
         '_mp_noRx.csv',
         '_mp_Rx.csv',
-         ]
+         ],
+#        [
+#        '_mp_noRx.csv',
+#        '_mp_Rx.csv',
+#         ],
+#        [
+#        '_mp_noRx.csv',
+#        '_mp_Rx.csv',
+#         ],
+        ]
 
 # Coefficients for Ps -> Pm
-# Pm = Ps*c[0] + c[1]
-# [mp_RX, mp_woRX]
+# Pm = Ps*c[X][0] + c[X][1]
+# X depends on pressure range [low, high]
+# c = [crossing_point, [mp_RX, mp_woRX]]
 conversion = [
-        [2.9, 0.05],
-        [2.5, 0.03],
+        [0.027, [1.1, 0], [2.9, 0.05]],
+        [0.023, [1.1, 0], [2.5, 0.03]],
         ]
 
 
@@ -65,35 +77,83 @@ def convert_spirometer_pressure_to_flow(pressure):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Calibration for this conversion is in
 # ~/Documents/Spirometry/python/maskLPF/Ps_Pm_calibration
+# This conversion rate only applies to expiration
 def convert_spirometer_pressure_to_mouth_pressure(spir_pressure, coeffs):
-    mouth_pressure = [coeffs[0]*p + coeffs[1] for p in spir_pressure]
+    for m in range(len(pressure)):
+        if(pressure[m] < coeffs[0]):
+            mouth_pressure = [coeffs[1][0]*p + coeffs[1][1] for p in spir_pressure]
+        else:
+            mouth_pressure = [coeffs[2][0]*p + coeffs[2][1] for p in spir_pressure]
+
     return mouth_pressure
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Resistance from mouth onwards is calculated
+def calculate_external_resistance(mouth_pressure, flow):
+    resistance = [0]*len(flow)
+    for m in range(len(mouth_pressure)):
+        try:
+            R = mouth_pressure[m]/flow[m]
+        except ZeroDivisionError:
+            R = np.nan
+        resistance[m] = R
+
+    plt.plot(mouth_pressure)
+    plt.plot(flow)
+    plt.plot(resistance)
+    plt.legend(['P', 'Q', 'R'])
+    plt.show()
+
+    return resistance
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+def calculate_decay_rates(pressure):
+    pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
 for j in range(len(subject)):
-    for i in range(len(files)):
-        pressure_lowRes = []
-        pressure_highRes = []
-        time = []
 
-        filename = path + subject[j] + '/' + subject[j] + files[i]
-        extract_data_arrays_from_file(filename, pressure_lowRes, pressure_highRes, time)
+    for i in range(len(test_type)):
+        decays = []
 
-        flow = convert_spirometer_pressure_to_flow(pressure_highRes)
-        mouth_pressure = convert_spirometer_pressure_to_mouth_pressure(pressure_highRes, conversion[i])
+        for k in range(2):
 
-        plt.plot(time, pressure_lowRes, 'b', linewidth=2)
-        plt.plot(time, pressure_highRes, 'k', linewidth=2)
-        plt.xlabel('Time(s)')
-        plt.ylabel('Pressure(Pa)')
-        plt.legend(['low_res', 'high_res'])
-        plt.show()
+            filename = path + subject[j] + '/normalised_' + subject[j] + test_type[i][k]
 
-        plt.plot(time, flow, 'b', linewidth=2)
-        plt.plot(time, pressure_highRes, 'm', linewidth=2)
-        plt.plot(time, mouth_pressure, 'k', linewidth=2)
-        plt.xlabel('Time(s)')
-        plt.ylabel('Pressure(Pa)')
-        plt.legend(['flow', 'high_res', 'mouth_pressure'])
-        plt.show()
+            try:
+                # Extract data to empty arrays.
+                # This will fail if file does not exist
+                pressure_lowRes = []
+                pressure_highRes = []
+                time = []
+                extract_data_arrays_from_file(filename, pressure_lowRes, pressure_highRes, time)
+
+                flow = convert_spirometer_pressure_to_flow(pressure_highRes)
+                mouth_pressure = convert_spirometer_pressure_to_mouth_pressure(pressure_highRes, conversion[k])
+                plt.plot(pressure_highRes)
+
+                external_resistance = calculate_external_resistance(mouth_pressure, flow)
+                calculate_decay_rates(spir_pressure)
+
+                plt.plot(time, pressure_lowRes, 'b', linewidth=2)
+                plt.plot(time, pressure_highRes, 'k', linewidth=2)
+                plt.xlabel('Time(s)')
+                plt.ylabel('Pressure(Pa)')
+                plt.legend(['low_res', 'high_res'])
+                plt.show()
+
+                plt.plot(time, flow, 'b', linewidth=2)
+                plt.plot(time, pressure_highRes, 'm', linewidth=2)
+                plt.plot(time, mouth_pressure, 'k', linewidth=2)
+                plt.xlabel('Time(s)')
+                plt.ylabel('Pressure(Pa)')
+                plt.legend(['flow', 'high_res', 'mouth_pressure'])
+                plt.show()
+
+            except IOError:
+                print("File {} does not exist".format(filename))
